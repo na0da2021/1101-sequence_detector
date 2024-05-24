f9f3e048d905544585244fc39988ddd392eab0e1
nadaomar <na0da2021@gmail.com>
2024-05-24 14:46:14 +0300

    done

---
+++ mealy/mealy_fsm_tb.v
@@ -0,0 +1,47 @@
+module mealy_fsm_tb();
+
+reg  in, clk, rst;
+wire f;
+
+fsm dut(.in(in), .clk(clk), .rst(rst), .f(f));
+
+initial begin
+in  = 1;
+clk = 0;
+rst = 0;
+
+#12
+rst = 1;
+in  = 1;
+
+#12
+rst = 0;
+in =  1;
+
+#12
+in = 0;
+
+#12
+in = 1;
+
+#12
+
+in = 1;
+
+#12
+in = 0;
+
+#12
+in = 1;
+
+#12
+in = 1;
+
+#12
+in = 0;
+end
+ 
+always #5 clk = ~ clk; 
+initial #500 $finish;  
+
+endmodule
